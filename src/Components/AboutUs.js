0b8710f677545150723cf9ce6b31591cee2e57c6
Shahad Alfaddagh <shahadalfaddagh@gmail.com>
2025-01-24 14:18:11 +0300

    Language change is now globally done in Hero, AboutUs, and NavBar components

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,95 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../Styles/AboutUs.scss";
 import BytechsPurple from "../assets/Bytechs-purple.png";
 import element from "../assets/elements.png";
 import target from "../assets/target-02.png";
+import { useLanguage } from "../utils/LanguageContext"; // Import the custom hook
 
 const AboutUs = () => {
-  const [activeLanguage, setActiveLanguage] = useState("en");
-
-  useEffect(() => {
-    const handleLanguageSwitch = (event) => {
-      const { lang } = event.detail;
-      setActiveLanguage(lang);
-    };
-
-    // Listen for language changes (if triggered globally)
-    window.addEventListener("languageChange", handleLanguageSwitch);
-    return () =>
-      window.removeEventListener("languageChange", handleLanguageSwitch);
-  }, []);
+  const { language } = useLanguage(); // Access the active language from context
 
   return (
     <section id="about" className="about">
       <div className="container">
-        {/* <!-- Illustration Images --> */}
-
+        {/* Content */}
         <div
           className={`content ${
-            activeLanguage === "ar" ? "align-right" : "align-left"
+            language === "ar" ? "align-right" : "align-left"
           }`}
         >
           <h2 data-en="What is Bytechs?" data-ar="ماذا يعني بايتكس؟">
-            What is Bytechs?
+            {language === "en" ? "What is Bytechs?" : "ماذا يعني بايتكس؟"}
           </h2>
           <p
             data-en="An initiative reshaping the world of technology by creating a sustainable ecosystem to activate the technical movement in artificial intelligence and emerging technologies. We support innovation, entrepreneurship, and applied research to contribute to achieving the three pillars of Saudi Vision 2030: an ambitious nation, a vibrant society, and a thriving economy."
-            data-ar=" للحصول على مزيد من المعلومات حول BytechS اتصل على: BytechS.contact@bytechs.net"
+            data-ar="مبادرة تعيد تشكيل عالم التقنية من خلال إنشاء نظام بيئي مستدام لتعزيز الحركة التقنية في الذكاء الاصطناعي والتقنيات الناشئة. نحن ندعم الابتكار وريادة الأعمال والبحوث التطبيقية للمساهمة في تحقيق ركائز رؤية المملكة 2030: وطن طموح، مجتمع نابض بالحياة، واقتصاد مزدهر."
           >
-            An initiative reshaping the world of technology by creating a
-            sustainable ecosystem to activate the technical movement in
-            artificial intelligence and emerging technologies. We support
-            innovation, entrepreneurship, and applied research to contribute to
-            achieving the three pillars of Saudi Vision 2030: an ambitious
-            nation, a vibrant society, and a thriving economy.
+            {language === "en"
+              ? "An initiative reshaping the world of technology by creating a sustainable ecosystem to activate the technical movement in artificial intelligence and emerging technologies. We support innovation, entrepreneurship, and applied research to contribute to achieving the three pillars of Saudi Vision 2030: an ambitious nation, a vibrant society, and a thriving economy."
+              : "مبادرة تعيد تشكيل عالم التقنية من خلال إنشاء نظام بيئي مستدام لتعزيز الحركة التقنية في الذكاء الاصطناعي والتقنيات الناشئة. نحن ندعم الابتكار وريادة الأعمال والبحوث التطبيقية للمساهمة في تحقيق ركائز رؤية المملكة 2030: وطن طموح، مجتمع نابض بالحياة، واقتصاد مزدهر."}
           </p>
 
           <div className="cards-wrapper">
             <div className="card mission">
               <img src={element} alt="shape1" />
               <h3 data-en="Our Mission" data-ar="مهمتنا">
-                Our Mission
+                {language === "en" ? "Our Mission" : "مهمتنا"}
               </h3>
               <p
                 className="card-content"
                 data-en="We aim to create a vibrant and sustainable tech community that enhances the Kingdom's position as a global leader in technology."
-                data-ar="We aim to create a vibrant and sustainable tech community that enhances the Kingdom's position as a global leader in technology."
+                data-ar="نسعى إلى إنشاء مجتمع تقني نابض بالحياة ومستدام يعزز مكانة المملكة كقائد عالمي في التكنولوجيا."
               >
-                We aim to create a vibrant and sustainable tech community that
-                enhances the Kingdom's position as a global leader in
-                technology.
+                {language === "en"
+                  ? "We aim to create a vibrant and sustainable tech community that enhances the Kingdom's position as a global leader in technology."
+                  : "نسعى إلى إنشاء مجتمع تقني نابض بالحياة ومستدام يعزز مكانة المملكة كقائد عالمي في التكنولوجيا."}
               </p>
             </div>
             <div className="card goal">
               <img src={target} alt="shape1" />
               <h3 data-en="Our Goals" data-ar="هدفنا">
-                Our Goals
+                {language === "en" ? "Our Goals" : "هدفنا"}
               </h3>
               <ul className="our-goal-list">
                 <li
-                  data-en="Build specialized tech communities focusing on interaction and
-                knowledge exchange."
-                  data-ar=""
+                  data-en="Build specialized tech communities focusing on interaction and knowledge exchange."
+                  data-ar="بناء مجتمعات تقنية متخصصة تركز على التفاعل وتبادل المعرفة."
                 >
-                  Build specialized tech communities focusing on interaction and
-                  knowledge exchange.
+                  {language === "en"
+                    ? "Build specialized tech communities focusing on interaction and knowledge exchange."
+                    : "بناء مجتمعات تقنية متخصصة تركز على التفاعل وتبادل المعرفة."}
                 </li>
                 <li
-                  data-en=" Provide specialized training programs that support technical and
-                professional skills."
-                  data-ar=""
+                  data-en="Provide specialized training programs that support technical and professional skills."
+                  data-ar="توفير برامج تدريبية متخصصة تدعم المهارات التقنية والمهنية."
                 >
-                  Provide specialized training programs that support technical
-                  and professional skills.
+                  {language === "en"
+                    ? "Provide specialized training programs that support technical and professional skills."
+                    : "توفير برامج تدريبية متخصصة تدعم المهارات التقنية والمهنية."}
                 </li>
                 <li
-                  data-en="Launch a digital platform that facilitates
-                collaboration and communication between tech enthusiasts and
-                experts."
-                  data-ar=""
+                  data-en="Launch a digital platform that facilitates collaboration and communication between tech enthusiasts and experts."
+                  data-ar="إطلاق منصة رقمية تسهل التعاون والتواصل بين عشاق التقنية والخبراء."
                 >
-                  Launch a digital platform that facilitates collaboration and
-                  communication between tech enthusiasts and experts.
+                  {language === "en"
+                    ? "Launch a digital platform that facilitates collaboration and communication between tech enthusiasts and experts."
+                    : "إطلاق منصة رقمية تسهل التعاون والتواصل بين عشاق التقنية والخبراء."}
                 </li>
               </ul>
             </div>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,14 +16,13 @@ const NavBar = () => {
     switchLanguage(newLanguage);
   };
 
+  // Update the background based on scroll position
   const handleScroll = useCallback(() => {
-    // Check section background color or scroll position
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
       const sectionBottom = section.getBoundingClientRect().bottom;
 
-      // Assuming section is visible when in viewport
       if (
         sectionTop <= window.innerHeight / 2 &&
         sectionBottom >= window.innerHeight / 2
@@ -31,78 +30,79 @@ const NavBar = () => {
         const backgroundColor =
           window.getComputedStyle(section).backgroundColor;
         const [r, g, b] = backgroundColor.match(/\d+/g).map(Number);
-
-        // Set threshold for "light" background (adjust as necessary)
         const isLight = (r + g + b) / 3 > 200;
         setIsLightBackground(isLight);
       }
     });
-  },[]);
+  }, []);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, [handleScroll]);
 
-  // Toggle burger menu visibility
-  const toggleBurgerMenu = () => {
-    setBurgerMenuOpen((prevState) => !prevState);
-  };
-
   return (
-    <nav style={{ color: isLightBackground ? "black" : "white" }}>
-      {/* <!-- Logo --> */}
-      <img
-        src={isLightBackground ? coloredLogo : logo}
-        className="logo"
-        alt="Bytechs Logo"
-      ></img>
+    <nav
+      style={{
+        color: isLightBackground ? "black" : "white",
+        direction: activeLanguage === "ar" ? "rtl" : "ltr",
+      }}
+      className={activeLanguage === "ar" ? "align-right" : "align-left"}
+    >
+      {/* Logo */}
+      <div className="left-container">
+        <img
+          src={isLightBackground ? coloredLogo : logo}
+          className="logo"
+          alt="Bytechs Logo"
+        />
 
-      {/* <!-- Navigation Links for Desktop --> */}
-      <ul className="nav-links">
-        <li>
-          <a
-            href="#home"
-            data-en="Home"
-            data-ar="الرئيسية"
-            style={{ color: isLightBackground ? "black" : "white" }}
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            data-en="Contact Us"
-            data-ar="اتصل بنا"
-            style={{ color: isLightBackground ? "black" : "white" }}
-          >
-            Contact Us
-          </a>
-        </li>
-        <li>
-          <a
-            href="#news"
-            data-en="News"
-            data-ar="الأخبار"
-            style={{ color: isLightBackground ? "black" : "white" }}
-          >
-            News
-          </a>
-        </li>
-        <li>
-          <a
-            href="#follow"
-            data-en="Follow Us"
-            data-ar="تابعنا"
-            style={{ color: isLightBackground ? "black" : "white" }}
-          >
-            Follow Us
-          </a>
-        </li>
-      </ul>
+        {/* Navigation Links */}
+        <ul className="nav-links">
+          <li>
+            <a
+              href="#home"
+              data-en="Home"
+              data-ar="الرئيسية"
+              style={{ textAlign: activeLanguage === "ar" ? "right" : "left" }}
+            >
+              {activeLanguage === "ar" ? "الرئيسية" : "Home"}
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              data-en="Contact Us"
+              data-ar="اتصل بنا"
+              style={{ textAlign: activeLanguage === "ar" ? "right" : "left" }}
+            >
+              {activeLanguage === "ar" ? "اتصل بنا" : "Contact Us"}
+            </a>
+          </li>
+          <li>
+            <a
+              href="#news"
+              data-en="News"
+              data-ar="الأخبار"
+              style={{ textAlign: activeLanguage === "ar" ? "right" : "left" }}
+            >
+              {activeLanguage === "ar" ? "الأخبار" : "News"}
+            </a>
+          </li>
+          <li>
+            <a
+              href="#follow"
+              data-en="Follow Us"
+              data-ar="تابعنا"
+              style={{ textAlign: activeLanguage === "ar" ? "right" : "left" }}
+            >
+              {activeLanguage === "ar" ? "تابعنا" : "Follow Us"}
+            </a>
+          </li>
+        </ul>
+      </div>
 
-      {/* <!-- Language Switch --> */}
+      {/* Language Switch */}
       <div className="language-switch">
         <button
           className={`Eng ${activeLanguage === "en" ? "active" : ""}`}
@@ -136,16 +136,13 @@ const NavBar = () => {
         >
           العربية
         </button>
-
-        <noscript>Please enable JavaScript to switch languages</noscript>
       </div>
 
       {/* Burger Menu */}
       <div
         className={`burger-menu ${burgerMenuOpen ? "active" : ""}`}
-        onClick={toggleBurgerMenu}
+        onClick={() => setBurgerMenuOpen((prevState) => !prevState)}
       >
-        {/* Burger Icon */}
         <div className="burger-icon">
           <span
             style={{ backgroundColor: isLightBackground ? "black" : "white" }}
@@ -157,48 +154,6 @@ const NavBar = () => {
             style={{ backgroundColor: isLightBackground ? "black" : "white" }}
           ></span>
         </div>
-
-        {/* Mobile Navigation Links */}
-        <ul className="burger-links">
-          <li>
-            <a href="#home" data-en="Home" data-ar="الرئيسية">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#contact" data-en="Contact Us" data-ar="اتصل بنا">
-              Contact Us
-            </a>
-          </li>
-          <li>
-            <a href="#news" data-en="News" data-ar="الأخبار">
-              News
-            </a>
-          </li>
-          <li>
-            <a href="#follow" data-en="Follow Us" data-ar="تابعنا">
-              Follow Us
-            </a>
-          </li>
-
-          {/* Language Switch inside Burger Menu */}
-          <div className="language-switch">
-            <button
-              className={`Eng ${activeLanguage === "en" ? "active" : ""}`}
-              onClick={() => toggleLanguage(activeLanguage)}
-            >
-              English
-            </button>
-            <button
-              className={`AR ${activeLanguage === "ar" ? "active" : ""}`}
-              onClick={() => toggleLanguage(activeLanguage)}
-            >
-              العربية
-            </button>
-
-            <noscript>Please enable JavaScript to switch languages</noscript>
-          </div>
-        </ul>
       </div>
     </nav>
   );
