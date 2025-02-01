93aa361f5a86ebf7c9dbbe037583484ae22cb941
Shahad Alfaddagh <shahadalfaddagh@gmail.com>
2025-02-01 22:12:07 +0300

    Communities content added

--- src/Components/Communities.js
+++ src/Components/Communities.js
@@ -2,70 +2,155 @@ import React, { useState } from "react";
 import "../Styles/Communities.scss";
 import CommunitiesData from "../CommunitiesData";
 import BYLogo from "../assets/BY-logo.png";
+import { useLanguage } from "../utils/LanguageContext";
 
 const tabs = [
-  "Vision & Objective",
-  "Why us",
-  "Distinctive",
-  "Audience",
-  "Activities",
-  "Structure",
+  { en: "Vision & Objective", ar: "الرؤية والهدف" },
+  { en: "Why us", ar: "لماذا نحن؟" },
+  { en: "Distinctive", ar: "ما يميزنا" },
+  { en: "Audience", ar: "الجمهور" },
+  { en: "Activities", ar: "الأنشطة" },
+  { en: "Structure", ar: "الهيكلة" },
 ];
 
 const Communities = () => {
   const [selectedCard, setSelectedCard] = useState(null);
   const [activeTab, setActiveTab] = useState(tabs[0]);
+  const { language } = useLanguage(); // Access the active language from context
 
   return (
-    <div className="cards-container">
-      <div className="left-panel">
-        {selectedCard ? (
-          <div className="card-details">
-            <div className="tabs">
-              <img src={selectedCard.image} alt={selectedCard.title} />
-              {tabs.map((tab) => (
-                <button
-                  key={tab}
-                  className={activeTab === tab ? "tab active" : "tab"}
-                  onClick={() => setActiveTab(tab)}
-                >
-                  {tab}
-                </button>
-              ))}
+    <section
+      id="communities"
+      className={`communities ${
+        language === "ar" ? "align-right" : "align-left"
+      }`}
+    >
+      <div className="cards-container">
+        <div className="left-panel">
+          {selectedCard ? (
+            <div className="card-details">
+              <div className="tabs">
+                <h1 className="card-title">
+                  {language === "ar"
+                    ? selectedCard.titleAr
+                    : selectedCard.title}
+                </h1>
+                <img
+                  className="card-lg-image"
+                  src={selectedCard.image}
+                  alt={selectedCard.title}
+                />
+                <div className="tabs-container">
+                  {tabs.map((tab) => (
+                    <button
+                      key={tab.en}
+                      className={activeTab === tab.en ? "tab active" : "tab"}
+                      onClick={() => setActiveTab(tab.en)}
+                    >
+                      {language === "ar" ? tab.ar : tab.en}
+                    </button>
+                  ))}
+                </div>
+                <div className="tab-content">
+                  {activeTab === "Vision & Objective" && (
+                    <>
+                      <h2>{language === "ar" ? "الهدف" : "Objective"}</h2>
+                      <p>
+                        {language === "ar"
+                          ? selectedCard.objectiveAr
+                          : selectedCard.objective}
+                      </p>
+                      <h2>{language === "ar" ? "الرؤية" : "Vision"}</h2>
+                      <p>
+                        {language === "ar"
+                          ? selectedCard.visionAr
+                          : selectedCard.vision}
+                      </p>
+                    </>
+                  )}
+                  {activeTab === "Why us" && (
+                    <>
+                      <h2>{language === "ar" ? "لماذا نحن؟" : "Why us"}</h2>
+                      <ul>
+                        {(language === "ar"
+                          ? selectedCard.whyUsAr
+                          : selectedCard.whyUs
+                        ).map((point, index) => (
+                          <li key={index}>{point}</li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
+                  {activeTab === "Distinctive" && (
+                    <>
+                      <h2>{language === "ar" ? "ما يميزنا" : "Distinctive"}</h2>
+                      <p>
+                        {language === "ar"
+                          ? selectedCard.distinctiveAr
+                          : selectedCard.distinctive}
+                      </p>
+                    </>
+                  )}
+                  {activeTab === "Audience" && (
+                    <>
+                      <h2>{language === "ar" ? "الجمهور" : "Audience"}</h2>
+                      <p>
+                        {language === "ar"
+                          ? selectedCard.audienceAr
+                          : selectedCard.audience}
+                      </p>
+                    </>
+                  )}
+                  {activeTab === "Activities" && (
+                    <>
+                      <h2>{language === "ar" ? "الأنشطة" : "Activities"}</h2>
+                      <ul>
+                        {(language === "ar"
+                          ? selectedCard.activitiesAr
+                          : selectedCard.activities
+                        ).map((point, index) => (
+                          <li key={index}>{point}</li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
+                  {activeTab === "Structure" && (
+                    <>
+                      <h2>{language === "ar" ? "الهيكلة" : "Structure"}</h2>
+                      <ul>
+                        {(language === "ar"
+                          ? selectedCard.structureAr
+                          : selectedCard.structure
+                        ).map((point, index) => (
+                          <li key={index}>{point}</li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className="tab-content">
-              {activeTab === "Vision & Objective" && (
-                <>
-                  <h2>Objective</h2>
-                  <p>{selectedCard.objective}</p>
-                  <h2>Vision</h2>
-                  <p>{selectedCard.vision}</p>
-                </>
-              )}
-              {activeTab === "Why us" && <p>{selectedCard.whyUs}</p>}
-              {activeTab === "Distinctive" && <p>{selectedCard.distinctive}</p>}
-              {activeTab === "Audience" && <p>{selectedCard.audience}</p>}
-              {activeTab === "Activities" && <p>{selectedCard.activities}</p>}
-              {activeTab === "Structure" && <p>{selectedCard.structure}</p>}
-            </div>
-          </div>
-        ) : (
-          <img src={BYLogo} alt="Large Logo" className="logo" />
-        )}
-      </div>
+          ) : (
+            <img src={BYLogo} alt="Large Logo" className="logo" />
+          )}
+        </div>
 
-      <div className="right-panel">
-        {CommunitiesData.map((card) => (
-          <div
-            key={card.id}
-            className="card"
-            onClick={() => setSelectedCard(card)}
-          >
-            <img src={card.image} alt={card.title} />
-          </div>
-        ))}
+        <div className="right-panel">
+          {CommunitiesData.map((card) => (
+            <div
+              key={card.id}
+              className="card"
+              onClick={() => setSelectedCard(card)}
+            >
+              <img src={card.image} alt={card.title} />
+              <h4 className="card-title">
+                {language === "ar" ? card.titleAr : card.title}
+              </h4>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
