5e8aec376b24537d2e9a0cf61b12893aec70a290
Shahad Alfaddagh <shahadalfaddagh@gmail.com>
2025-02-03 00:10:08 +0300

    Component's order has changed

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Hero from "./Components/Hero";
 import News from "./Components/News";
 import ContactUs from "./Components/ContactUs";
+import Communities from './Components/Communities';
 
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <ContactUs />
+      <Communities />
       <News />
+      <ContactUs />
     </div>
   );
 }
