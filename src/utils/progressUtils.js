fc1d3714fd48eed233dc66a703caca4503d8f31a
Shahad Alfaddagh <shahadalfaddagh@gmail.com>
2025-02-07 15:25:37 +0300

    Progress bar working now

--- src/utils/progressUtils.js
+++ src/utils/progressUtils.js
@@ -1,13 +1,21 @@
+// progressUtils.js
 export const animateProgressBar = (progressBarRef, progressNumberRef) => {
-  if (!progressBarRef.current || !progressNumberRef.current) return; // Prevent errors
   let progress = 0;
+
+  // Check if refs are defined before starting the interval
+  if (!progressBarRef.current || !progressNumberRef.current) {
+    console.error("Progress bar or progress number reference is null.");
+    return;
+  }
+
+  // eslint-disable-next-line no-unused-vars
   const interval = setInterval(() => {
     if (progress >= 100) {
-      clearInterval(interval);
+      progress = 0; // Reset progress without clearing the interval
     } else {
-      progress += 1;
+      progress++;
       progressBarRef.current.style.width = `${progress}%`;
       progressNumberRef.current.textContent = `${progress}%`;
     }
-  }, 50);
+  }, 100);
 };
