34a466ec0971d7160a0b287e18163739fccdd24e
abduulahaltamh <2230009012@iau.edu.sa>
2025-04-16 18:44:16 +0300

    fix:

--- src/Components/resultImageMap.js
+++ src/Components/resultImageMap.js
@@ -1,8 +1,12 @@
 const resultImageMap = {
-  TechInnovator: "https://bytechs.net/share/TechInnovator.html",
-  LogicalAnalyst: "https://bytechs.net/share/LogicalAnalyst.html",
-  PracticalUser: "https://bytechs.net/share/PracticalUser.html",
-  CuriousExplorer: "https://bytechs.net/share/CuriousExplorer.html",
+  TechInnovator:
+    "https://bytechs-platform.github.io/ByTechS-Website/share/TechInnovator.html",
+  LogicalAnalyst:
+    "https://bytechs-platform.github.io/ByTechS-Website/share/LogicalAnalyst.html",
+  PracticalUser:
+    "https://bytechs-platform.github.io/ByTechS-Website/share/PracticalUser.html",
+  CuriousExplorer:
+    "https://bytechs-platform.github.io/ByTechS-Website/share/CuriousExplorer.html",
 };
 
 export default resultImageMap;
