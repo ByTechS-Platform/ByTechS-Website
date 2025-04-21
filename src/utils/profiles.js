289b478616a5b49d314b295919525c4d0cb9789c
abduulahaltamh <2230009012@iau.edu.sa>
2025-04-21 22:26:19 +0300

    feat:

--- src/utils/profiles.js
+++ src/utils/profiles.js
@@ -1,7 +1,7 @@
 import image1 from "../assets/images/Tech.png";
 import image2 from "../assets/images/Logical.png";
 import image3 from "../assets/images/Practical.png";
-import image4 from "../assets/images/Curious.png";
+import image4 from "../assets/images/NEW.png";
 
 export const charactersImages = {
   TechInnovator: image1,
